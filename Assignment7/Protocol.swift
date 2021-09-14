9991bf5e3c806825592ada68d4df7a8282ac796d
Sharapatttt <75223431+Sharapatttt@users.noreply.github.com>
2021-09-15 02:43:46 +0600

    init

---
+++ Assignment7/Protocol.swift
@@ -0,0 +1,14 @@
+//
+//  Protocol.swift
+//  Assignment7
+//
+//  Created by Azamat Sharapat on 11/6/20.
+//  Copyright © 2020 Azamat Sharapat All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol Protocol {
+    func changeText(cityTitle: String, placeTitle: String, index: Int)
+}
